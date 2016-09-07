a9d058d19a42ff80f70c26b2e5a4ad6833e988f9
Friso <friso.obbema@gmail.com>
2016-09-07 13:33:01 +0200

    Map
    met de naam scripts

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Main {
+
+	public static int score;
+	public static int lives;
+
+	public static void InitGame () {
+		
+		score = 0;
+		lives = 3;
+	}
+
+}
